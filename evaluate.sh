5286ec437ff0e0cadcaad7d3c12c7bfd7940f581
Mirko Lenz <mirko@mirkolenz.com>
2022-07-29 19:09:42 +0200

    Enable parallel running of eval

--- evaluate.sh
+++ evaluate.sh
@@ -3,6 +3,6 @@
 alias app="poetry run python -m argmining_clustering"
 
 for glob in "microtexts/*.json" "essays/*.ann" "kialo-small/*.txt"; do
-    app "$glob"
-    app "$glob" --predict-mc
+    app "$glob" &
+    app "$glob" --predict-mc &
 done
