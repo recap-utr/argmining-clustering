f18569a6e9cd58e8078c787c82e8df1719849e50
Mirko Lenz <mirko@mirkolenz.com>
2022-07-29 19:05:19 +0200

    Add script for evaluation

--- argmining_clustering/__main__.py
+++ argmining_clustering/__main__.py
@@ -30,88 +30,84 @@ def run(
     model: str = "en_core_web_lg",
 ):
     features.load_spacy(model)
+    params = locals()
+    global_eval: dict[str, dict[str, list[float]]] = defaultdict(
+        lambda: defaultdict(list)
+    )
+    cases = serialization.load(input_folder, input_patterns)
 
-    for input_pattern in input_patterns:
-        params = locals()
-        global_eval: dict[str, dict[str, list[float]]] = defaultdict(
-            lambda: defaultdict(list)
+    for path, original_graph in track(cases.items()):
+        # For now, we do not consider the schemes between atom nodes during the evaluation
+        original_stripped_graph = original_graph.copy().strip_scheme_nodes()
+
+        index2id = dict(enumerate(original_graph.atom_nodes))
+        id2index = {v: k for k, v in index2id.items()}
+
+        mc = original_graph.major_claim or original_graph.root_node
+        assert mc is not None
+
+        mc_index = None if predict_mc else id2index[mc.id]
+        atom_nodes = list(original_graph.atom_nodes.values())
+
+        runner = Runner(atom_nodes, mc_index, invert_sim)
+        local_eval: defaultdict[str, list[tuple[ag.Graph, ag.Graph]]] = defaultdict(
+            list
         )
-        cases = serialization.load(input_folder, [input_pattern])
 
-        for path, original_graph in track(cases.items()):
-            # For now, we do not consider the schemes between atom nodes during the evaluation
-            original_stripped_graph = original_graph.copy().strip_scheme_nodes()
-
-            index2id = dict(enumerate(original_graph.atom_nodes))
-            id2index = {v: k for k, v in index2id.items()}
-
-            mc = original_graph.major_claim or original_graph.root_node
-            assert mc is not None
-
-            mc_index = None if predict_mc else id2index[mc.id]
-            atom_nodes = list(original_graph.atom_nodes.values())
-
-            runner = Runner(atom_nodes, mc_index, invert_sim)
-            local_eval: defaultdict[str, list[tuple[ag.Graph, ag.Graph]]] = defaultdict(
-                list
+        for method_name, method in runner.methods.items():
+            start = time()
+            clustering = method()
+            end = time()
+            reconstructed_graph = reconstruction.argument_graph(
+                original_graph.atom_nodes, index2id, clustering
             )
 
-            for method_name, method in runner.methods.items():
-                start = time()
-                clustering = method()
-                end = time()
-                reconstructed_graph = reconstruction.argument_graph(
-                    original_graph.atom_nodes, index2id, clustering
+            if output_folder:
+                serialization.save(
+                    reconstructed_graph,
+                    output_folder / path / method_name,
+                    render=True,
                 )
 
-                if output_folder:
-                    serialization.save(
-                        reconstructed_graph,
-                        output_folder / path / method_name,
-                        render=True,
-                    )
+            reconstructed_graph.strip_scheme_nodes()
+            local_eval[method_name].append(
+                (original_stripped_graph, reconstructed_graph)
+            )
+            global_eval[method_name]["duration"].append(
+                (end - start) * 1000
+            )  # store in ms
 
-                reconstructed_graph.strip_scheme_nodes()
-                local_eval[method_name].append(
-                    (original_stripped_graph, reconstructed_graph)
-                )
-                global_eval[method_name]["duration"].append(
-                    (end - start) * 1000
-                )  # store in ms
+        for clustering_name, values in local_eval.items():
+            avg = evaluation.avg(clustering_name, values)
 
-            for clustering_name, values in local_eval.items():
-                avg = evaluation.avg(clustering_name, values)
+            for eval_func_name, eval_func_value in avg.items():
+                global_eval[clustering_name][eval_func_name].append(eval_func_value)
 
-                for eval_func_name, eval_func_value in avg.items():
-                    global_eval[clustering_name][eval_func_name].append(eval_func_value)
+    funcs = ["duration"] + sorted(
+        [func_name for func_name in evaluation.FUNCTIONS.keys()]
+    )
+    console = Console(file=StringIO(), width=88)
+    console.print(params)
 
-        funcs = ["duration"] + sorted(
-            [func_name for func_name in evaluation.FUNCTIONS.keys()]
-        )
-        console = Console(file=StringIO(), width=88)
-        console.print(params)
+    for aggregation in (mean, min, max, median):
+        table = Table(title=aggregation.__name__)
+        table.add_column("Method")
 
-        for aggregation in (mean, min, max, median):
-            table = Table(title=aggregation.__name__)
-            table.add_column("Method")
+        for func in funcs:
+            table.add_column(func, justify="right")
 
-            for func in funcs:
-                table.add_column(func, justify="right")
+        for clustering_name, eval in global_eval.items():
+            values = ["{:.3f}".format(aggregation(eval[func])) for func in funcs]
+            table.add_row(clustering_name, *values)
 
-            for clustering_name, eval in global_eval.items():
-                values = ["{:.3f}".format(aggregation(eval[func])) for func in funcs]
-                table.add_row(clustering_name, *values)
+        console.print(table)
 
-            console.print(table)
+    printed_content = t.cast(StringIO, console.file).getvalue()
+    print(printed_content)
 
-        printed_content = t.cast(StringIO, console.file).getvalue()
-        print(printed_content)
-
-        console_path = Path(
-            "./data/output/", pendulum.now().format("YYYY-MM-DD-HH-mm-ss")
-        ).with_suffix(".txt")
-        with console_path.open("w") as f:
-            f.write(printed_content)
+    console_path = Path("./data/output/", ag.uuid()).with_suffix(".txt")
+    with console_path.open("w") as f:
+        f.write(printed_content)
 
 
 if __name__ == "__main__":
---
+++ evaluate.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+alias app="poetry run python -m argmining_clustering"
+
+for glob in "microtexts/*.json" "essays/*.ann" "kialo-small/*.txt"; do
+    app "$glob"
+    app "$glob" --predict-mc
+done
