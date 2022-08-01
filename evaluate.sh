eeb5fb3100d98398f7190c433f89ceda9097ac63
Mirko Lenz <mirko@mirkolenz.com>
2022-08-01 14:24:09 +0200

    Remove preset mc from evaluation template

--- evaluate.sh
+++ evaluate.sh
@@ -3,7 +3,6 @@
 alias app="poetry run python -m argmining_clustering --no-progress"
 
 for glob in "microtexts/*.json" "essays/*.ann" "kialo-small/*.txt"; do
-    app "$glob" &
     app "$glob" --predict-mc &
 done
 wait
